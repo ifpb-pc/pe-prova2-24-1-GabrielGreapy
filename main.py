c79db7cd2dca0a9fbbfd1a5215938b157b5e6a7b
GabrielGreapy <159476031+GabrielGreapy@users.noreply.github.com>
2024-09-19 19:37:38 +0000

    derrota

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from collections import Counter
 from typing import List
+import csv
 def q1a(nums: List[int]) -> bool:
     pass
+    
 
 def q1b(nums: List[int]) -> bool:
     pass
@@ -9,14 +11,32 @@ def q1b(nums: List[int]) -> bool:
 
 def q2(s: List,t: List) -> bool:
     pass
-
+    
 def q3() -> int:
-    pass
+    s = input('')
+    t = input('')
+    if s.sort() == t.sort() :
+        return(True)
 
 def q4():
-    pass
+    lista1 = []
+    num = int(input(''))
+
+    while num != 0:
+        num = int(input(''))
+        lista1.append(num)
+    resultado_impares = []
+    resultado_pares = []
+
+    for i in lista1:
+        if i % 2 == 0:
+            resultado_pares.append(i)
+        else : 
+            resultado_impares.append(i)
+    return(resultado_pares, resultado_impares)
+    
 
 
 if __name__=="__main__":
     # teste sua questão manualmente aqui
-    pass
+    q3()
